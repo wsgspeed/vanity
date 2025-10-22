d0ad27327ada247b7f8f15823dff0ba21d2572f3
wsgspeed <speedspersonalacc@gmail.com>
2025-10-21 21:21:03 -0400

    Add new key to keyDatabase in checkKey.js

--- pages/api/checkKey.js
+++ pages/api/checkKey.js
@@ -1,6 +1,7 @@
 let keyDatabase = {
   "developer-kwmeah-oenajcu-3l9a8f5": false, // S_0
   "developer-ajwnfg-pwnahd-3n1s8gf": false   // ROSARY
+  "vanity-sjffwjada-192fad74-fnaw35gfa": false // name
 };
 
 export default function handler(req, res) {
@@ -22,3 +23,4 @@ export default function handler(req, res) {
 
   return res.json({ result: 'validKey' });
 }
+
